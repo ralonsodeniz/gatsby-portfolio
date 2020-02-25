d7539203cceda0694cf30e550857272baf23ac51
ralonsodeniz <ralonsodeniz@gmail.com>
2020-02-25 23:20:37 +0100

    fixed texts

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,24 +21,26 @@ const Header = () => {
   }, []);
 
   return (
-    <section id="hero" className="jumbotron">
-      <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title} <span className="text-color-main">{name}</span>
-            <br />
-            {subtitle || "I'm the Unknown Developer."}
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <a className="cta-btn cta-btn--hero" href="#about">
-              {cta}
-            </a>
-          </p>
-        </Fade>
-      </Container>
-    </section>
+    hero && (
+      <section id="hero" className="jumbotron">
+        <Container>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+            <h1 className="hero-title">
+              {title} <span className="text-color-main">{name}</span>
+              <br />
+              {subtitle}
+            </h1>
+          </Fade>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+            <p className="hero-cta">
+              <a className="cta-btn cta-btn--hero" href="#about">
+                {cta}
+              </a>
+            </p>
+          </Fade>
+        </Container>
+      </section>
+    )
   );
 };
 
