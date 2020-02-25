b71e94a995205ff6f0b8974c4f0dd453c885d072
ralonsodeniz <ralonsodeniz@gmail.com>
2020-02-25 23:25:21 +0100

    fixed texts

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,7 +21,7 @@ const Header = () => {
   }, []);
 
   return (
-    hero && (
+    title && (
       <section id="hero" className="jumbotron">
         <Container>
           <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
